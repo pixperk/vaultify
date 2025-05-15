840b7782daf2169a7196f2eb0128c8131eeb8568
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-05-15 21:05:02 +0530

    fix(secrets) : unique constraint violation

--- internal/api/secrets.go
+++ internal/api/secrets.go
@@ -1,9 +1,12 @@
 package api
 
 import (
+	"database/sql"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	"github.com/pixperk/vaultify/internal/auth"
 	db "github.com/pixperk/vaultify/internal/db/sqlc"
 )
@@ -28,6 +31,20 @@ func (s *Server) createSecret(ctx *gin.Context) {
 
 	authPayload := ctx.MustGet(authorizationPayloadKey).(*auth.Payload)
 
+	if authPayload == nil {
+		ctx.JSON(401, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	user, err := s.store.GetUserByEmail(ctx, authPayload.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(404, gin.H{"error": "user not found"})
+			return
+		}
+		ctx.JSON(500, gin.H{"error": "internal server error"})
+	}
+
 	// Encrypt the secret value
 	encryptedValue, nonce, err := s.encryptor.Encrypt([]byte(req.Value))
 	if err != nil {
@@ -36,14 +53,30 @@ func (s *Server) createSecret(ctx *gin.Context) {
 	}
 
 	arg := db.CreateSecretParams{
-		UserID: authPayload.ID,
-		Path:   fmt.Sprintf("%s/%s", authPayload.Email, req.Path),
+		UserID:         user.ID,
+		Path:           fmt.Sprintf("%s/%s", authPayload.Email, req.Path),
+		EncryptedValue: encryptedValue,
+		Nonce:          nonce,
+	}
+
+	secret, err := s.store.CreateSecret(ctx, arg)
+
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
 	}
 
 	resp := secretResponse{
 		Path:      arg.Path,
-		Encrypted: encryptedValue, // return encrypted value for client use
-		Nonce:     nonce,          // return nonce hex encoded for client use (optional)
+		Encrypted: secret.EncryptedValue,
+		Nonce:     secret.Nonce,
 	}
 
 	ctx.JSON(200, resp)
