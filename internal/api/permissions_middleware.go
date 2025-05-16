ddf3a2c98fb234ea1bc18ef0950ebe23dbcb3d1a
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-05-16 12:30:27 +0530

    feat(share) : write access middleware

--- internal/api/permissions_middleware.go
+++ internal/api/permissions_middleware.go
@@ -52,3 +52,49 @@ func (s *Server) RequireReadAccess() gin.HandlerFunc {
 		ctx.AbortWithStatusJSON(403, gin.H{"error": "Access denied"})
 	}
 }
+
+func (s *Server) RequireWriteAccess() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		rawPath := ctx.Param("path")
+		path := strings.TrimPrefix(rawPath, "/") // remove leading slash
+		authPayload := ctx.MustGet(authorizationPayloadKey).(*auth.Payload)
+
+		secret, err := s.store.GetSecretByPath(ctx, path)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				ctx.AbortWithStatusJSON(404, gin.H{"error": "Secret not found"})
+			} else {
+				ctx.AbortWithStatusJSON(500, gin.H{"error": "Error fetching secret"})
+			}
+			return
+		}
+
+		// Check if the user is owner
+		if secret.UserID == authPayload.UserID {
+			ctx.Set("secret", secret)
+			ctx.Next()
+			return
+		}
+
+		// Check if shared
+		permissions, err := s.store.GetPermissions(ctx, db.GetPermissionsParams{
+			Path:        path,
+			TargetEmail: authPayload.Email,
+		})
+
+		if err != nil {
+
+			ctx.AbortWithStatusJSON(403, gin.H{"error": "Access denied"})
+			return
+		}
+
+		// Check if the user has write access
+		if permissions == "write" {
+			ctx.Set("secret", secret)
+			ctx.Next()
+			return
+		}
+
+		ctx.AbortWithStatusJSON(403, gin.H{"error": "Access denied"})
+	}
+}
